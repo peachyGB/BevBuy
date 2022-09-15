d72eb0cbbfaf4636bf2ffaf1cbd0756952824118
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-09-15 12:35:21 -0400

    database url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_023827) do
+ActiveRecord::Schema.define(version: 2022_07_15_143857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,11 +40,12 @@ ActiveRecord::Schema.define(version: 2022_07_11_023827) do
     t.integer "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.float "wallet"
+    t.integer "wallet"
     t.integer "member_points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
